9ee0b9036c749ee95daf3b38cbe3113e222ecbdb
David <rynstheoverlord@gmail.com>
2026-01-30 00:06:22 +0100

    added interactive form when using 'vial create'

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/common-nighthawk/go-figure"
+	// "vial/filesystem"
 	"vial/filesystem"
 	"vial/logger"
+
+	"github.com/charmbracelet/huh"
+	"github.com/common-nighthawk/go-figure"
 )
+
+
+type Configuration struct {
+	projectTitle string 
+	projectStyle string 
+	packageManager string
+	installAndStartServer bool
+}
 
 func main() {
 	var newLogger = logger.NewLogger()
@@ -19,23 +31,57 @@ func main() {
 	}
 
 	if arg == "create" {
-		project_name, err := getArg(2)
-		if err != nil {
-			newLogger.Error("Not enough arguments to execute command!")
-			fmt.Println("Hint: Use vial create <project-name> instead!")
-			return
-		}
+		var projectConfig = createProjectForm()
 
-		filesystem.CreateProject(project_name)
-		newLogger.Success(fmt.Sprintf("%v: Project created successfully!", project_name))
+		filesystem.CreateProject(projectConfig.projectTitle)
+		newLogger.Success(fmt.Sprintf("%v: Project created successfully!", projectConfig.projectTitle))
 	} else if arg == "help" || arg == "h" || arg == "-h" || arg == "--help" {
 		newLogger.Info("HELP")
-		fmt.Println(`help             => Displays this message
-create <name>    => Creates new app`)
+		fmt.Println(`help 		=> Displays this message
+create 		=> Creates new app`)
 	} else if arg == "audit" {
 
 	} else {
 		newLogger.Error("Not a valid command!")
 		fmt.Println("Use 'help' to get the list of commands!")
 	}
+}
+
+
+func createProjectForm() Configuration {
+	var config Configuration 
+	var form = huh.NewForm(
+		huh.NewGroup( 
+			huh.NewInput().Title("Project name?").Value(&config.projectTitle). 
+				Validate(func(s string) error { 
+					if len(s) > 0 {
+						return nil 
+					} else {
+						return fmt.Errorf("Project title cannot be left empty!")
+					}
+				}),
+		),
+		huh.NewGroup(	
+			huh.NewSelect[string]().Title("Select project type"). 
+			Options( 
+				huh.NewOption("Simple", "simple"), 
+				huh.NewOption("Standard", "standard"), 
+				huh.NewOption("Complex", "complex"),
+			).Value(&config.projectStyle),
+			),
+
+		huh.NewGroup(
+			huh.NewSelect[string]().Title("Select package manager"). 
+			Options( 
+				huh.NewOption("Python Installer Package (pip)", "pip"), 
+				huh.NewOption("UV", "uv"), 
+			).Value(&config.packageManager),
+		),
+	)
+
+	if err := form.Run(); err != nil {
+		logger.NewLogger().Error("Process exited!")
+		os.Exit(1)
+	}
+	return config
 }
